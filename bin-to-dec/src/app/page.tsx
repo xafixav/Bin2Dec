b5c9238fb804a4332293eda02a4c3776f7406cb7
Gustavo Andrade <xafiworld@gmail.com>
2023-11-16 19:05:36 -0300

    fix css

--- bin-to-dec/src/app/page.tsx
+++ bin-to-dec/src/app/page.tsx
@@ -1,14 +1,15 @@
-import Magic from "../../components/input"
+
+
+import Magic from "./components/input"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
 
-        <div className="text-5xl pb-[25%]">
+        <div className="text-5xl pb-[15%] text-center">
           Binary to Decimal Converter
         </div>
-        <p className="pb-[50px] flex flex-col items-center text-3xl">Type Binaries (1 or 0)</p>
-
+        <p className="pt-[2rem] pb-[1rem] flex flex-col items-center text-3xl">Type Binaries (1 or 0)</p>
 
         <Magic />
       </div>
